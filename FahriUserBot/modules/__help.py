8ffe0061142f43c4af726f590781795107c7ef41
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-10-08 08:15:00 +0800

    Update __help.py

--- FahriUserBot/modules/__help.py
+++ FahriUserBot/modules/__help.py
@@ -1,8 +1,8 @@
 import logging
 
 
-from userbot import BOT_USERNAME
-from userbot.events import register
+from FahriUserBot import BOT_USERNAME
+from FahriUserBot.events import register
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
