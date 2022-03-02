d72e78b17a7e9217a706c21a4c457eefaf817ed2
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2022-03-02 19:51:29 +0800

    Update help.py

--- FahriUserBot/modules/help.py
+++ FahriUserBot/modules/help.py
@@ -18,21 +18,22 @@ async def help(Alvin):
             await Alvin.delete()
     else:
         await Alvin.edit("☠️")
+        await asyncio.sleep(3)
         await Alvin.edit("**☠️MODULES☠️:**\n"
-                        "`admin` | `adzan` | `afk` | `bored` | `vip` | `animation`|  `android` | `anime` | `anti_spambot` | `aria` | `ascii`|\n"
-                        "`blacklist` | `carbon`  | `chat` | `mutechat` | `corona` | `creating` | `deepfry` | `emogames`|\n"
-                        "`eval` | `exec` | `term` | `fgban` | `federation` | `figlet` | `tags` | `filters` | `gban` | `gcast` | `googledrive` | `gcommit` | `github`|\n"
-                        "`glitch` | `gps` | `hash` | `base64` | `hentai` | `heroku` | `id` | `memeimage` | `power`|\n"
-                        "`lastfm` | `locks` | `master` | `aeshtetic` | `detect` | `mastergame` | `helpers` | `hazmat`|\n"
-                        "`instagram` | `impostor` | `memes` | `misc` | `app` | `undelete` | `grab` | `clone`|\n"
-                        "`randomprofil` | `song` | `tiny` | `tempmail` | `tiktok` | `wordcloud`|\n" 
-                        "`lyrics` | `mega` | `justforfun` | `memify` | `mentions` | `purge` | `purgeme` | `del` | `edit`|\n"
-                        "`sd` | `random` | `sleep` | `shutdown` | `repo`  |`readme` | `repeat` | `restart`|\n"
-                        "`raw` | `nekobot` | `notes` | `off` | `phreaker` | `pm` | `profil` | `quotly`|  `rastick` | `resi` | `reverse` | `salam` | `sangmata`|\n"
-                        "`santet` | `image_search` | `currency` | `google` | `wiki`| `ud` | `tts` | `translate`| `youtube` | `rip`|\n"
-                        "`removebg` | `ocr` | `qrcode` | `barcode` | `paste` | `getpaste` | `nekobin` | `direct` | `screenshot` | `sed` | `snips` | `spammer` | `spotifynow` | `ssvideo`|\n"
-                        "`stickers` | `stickers2` | `sysd` | `botver` | `pip` | `alive` | `tag_all` | `telegraph` | `timedate` | `torrent`|\n" 
-                        "`transform` | `updater` |`downloader` | `waifu` | `wallpaper` | `weather`|\n"
+                        "`admin` | `adzan` | `afk` | `bored` | `vip` | `animation`|  `android` | `anime` | `anti_spambot` | `aria` | `ascii`|"
+                        "`blacklist` | `carbon`  | `chat` | `mutechat` | `corona` | `creating` | `deepfry` | `emogames`|"
+                        "`eval` | `exec` | `term` | `fgban` | `federation` | `figlet` | `tags` | `filters` | `gban` | `gcast` | `googledrive` | `gcommit` | `github`|"
+                        "`glitch` | `gps` | `hash` | `base64` | `hentai` | `heroku` | `id` | `memeimage` | `power`|"
+                        "`lastfm` | `locks` | `master` | `aeshtetic` | `detect` | `mastergame` | `helpers` | `hazmat`|"
+                        "`instagram` | `impostor` | `memes` | `misc` | `app` | `undelete` | `grab` | `clone`|"
+                        "`randomprofil` | `song` | `tiny` | `tempmail` | `tiktok` | `wordcloud`|" 
+                        "`lyrics` | `mega` | `justforfun` | `memify` | `mentions` | `purge` | `purgeme` | `del` | `edit`|"
+                        "`sd` | `random` | `sleep` | `shutdown` | `repo`  |`readme` | `repeat` | `restart`|"
+                        "`raw` | `nekobot` | `notes` | `off` | `phreaker` | `pm` | `profil` | `quotly`|  `rastick` | `resi` | `reverse` | `salam` | `sangmata`|"
+                        "`santet` | `image_search` | `currency` | `google` | `wiki`| `ud` | `tts` | `translate`| `youtube` | `rip`|"
+                        "`removebg` | `ocr` | `qrcode` | `barcode` | `paste` | `getpaste` | `nekobin` | `direct` | `screenshot` | `sed` | `snips` | `spammer` | `spotifynow` | `ssvideo`|"
+                        "`stickers` | `stickers2` | `sysd` | `botver` | `pip` | `alive` | `tag_all` | `telegraph` | `timedate` | `torrent`|" 
+                        "`transform` | `updater` |`downloader` | `waifu` | `wallpaper` | `weather`|"
                         "`webupload` | `welcome`|  `info` | `ping` | `signal` | `xiaomi` | `zipfile`")
         await Alvin.reply("\n**HOW TO USE????,** **EXAMPLE:**\n**TYPE** `;help afk` **FOR INFORMATION MODULES**GO TO**GROUP SUPPORT**\n[CLICK HERE TO GO](t.me/Alvin_Userbot_Group)")
         await asyncio.sleep(1000)
