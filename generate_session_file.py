c8643c74342a64c6ec7ce33d819e80f28c2e1f56
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2022-03-09 20:47:57 +0800

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -7,5 +7,5 @@ load_dotenv("config.env")
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 
-bot = TelegramClient('userbot', API_KEY, API_HASH)
+bot = TelegramClient('FahriUserBot', API_KEY, API_HASH)
 bot.start()
